0494e9ca433be6bedeab0a10b1ac10506e31617d
Megly <ec2-user@ip-172-31-11-140.us-east-2.compute.internal>
2018-07-04 01:58:35 +0000

    first migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
             $table->string('password');
+            $table->integer('team');
+            $table->integer('sex');
+            $table->integer('size');
+            $table->integer('hot_cold')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
