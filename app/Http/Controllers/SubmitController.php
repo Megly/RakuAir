5265e99e9909289bc305268c70f62bc411f6ec9e
shota.hanaoka <ec2-user@ip-172-31-11-154.us-east-2.compute.internal>
2018-07-05 04:06:30 +0000

    try1

---
+++ app/Http/Controllers/SubmitController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+
+class SubmitController extends Controller
+{
+    use RegistersUsers;
+
+
+    public function submit(){
+        echo "あいう";
+    }
+    
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'feel' => 'required|string|max:255',
+            
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'feel' => $data['feel']
+            
+        ]);
+    }
+    
+    
+}
--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'password', 'team', 'sex', 'size'
+        'name', 'password', 'team', 'sex', 'size','feel'
     ];
 
     /**
