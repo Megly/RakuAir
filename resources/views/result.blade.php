8e5d648cab998a677fda2fa43d43284453203356
kazuho.sato <ec2-user@ip-172-31-14-23.us-east-2.compute.internal>
2018-07-10 02:56:59 +0000

    second gurahu

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <?php 
 	$result = DB::table('users') ->sum('feel');
-	$hot = DB::table('users') ->where('feel','1') ->sum('feel');
-	$cold = DB::table('users') ->where('feel','-1') ->sum('feel');
+	$hot = DB::table('users') ->where('feel','>=','0') ->sum('feel');
+	$cold = DB::table('users') ->where('feel','<=','0') ->sum('feel');
     $hotcount = DB::table('users') ->where('feel','>=','0') ->count('feel');
 	$coldcount = DB::table('users') ->where('feel','<=','0') ->count('feel');
 ?>     
@@ -14,7 +14,7 @@ google.setOnLoadCallback(drawChart);
 function drawChart() {
 var data = google.visualization.arrayToDataTable([ //グラフデータの指定
         ['Task', 'Hours per Day'],
-        ['Cold',      <?php echo $coldcount ?>],
+        ['Cold',    <?php echo $coldcount ?>],
         ['Hot',     <?php echo $hotcount ?>]
      
        
@@ -28,6 +28,29 @@ var chart = new google.visualization.PieChart(document.getElementById('piechart'
 }
 </script>
 <div id="piechart"></div>
+
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
+    google.load("visualization", "1", {packages:["corechart"]});
+    google.setOnLoadCallback(
+        function() {
+            var data = google.visualization.arrayToDataTable([
+                [       '', 'Cold', 'Hot'],
+                ['now', <?php echo abs($cold) ?>,  <?php echo $hot ?>       ]
+            
+            ]);
+    
+            var options = {
+                title: '会社業績',
+                hAxis: {title: '年度'}
+            };
+    
+            var chart = new google.visualization.ColumnChart(document.getElementById('gct_sample_column'));
+            chart.draw(data, options);
+        }
+    );
+</script>
+<div id="gct_sample_column" style="width:80%; height:250pt" ></div>
             
 
 @endsection
