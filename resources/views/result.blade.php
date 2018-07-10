ac07a8f82cfddd0c91a477ae2d056750abaf9fa1
shota.hanaoka <ec2-user@ip-172-31-11-154.us-east-2.compute.internal>
2018-07-10 04:02:42 +0000

    eekann

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,13 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<?php 
+<h1><?php 
 	$result = DB::table('users') ->sum('feel');
 	$hot = DB::table('users') ->where('feel','>=','0') ->sum('feel');
 	$cold = DB::table('users') ->where('feel','<=','0') ->sum('feel');
     $hotcount = DB::table('users') ->where('feel','>=','0') ->count('feel');
 	$coldcount = DB::table('users') ->where('feel','<=','0') ->count('feel');
-?>     
+	$gap = $hot + $cold;
+	
+if($gap>=20){
+	    echo '推奨温度22度';
+    	}elseif($gap <= -20){
+	    echo '推奨温度26度';;
+    	}else{
+	    echo 'ええ感';
+	    }
+	
+
+?> </h1>    
 
 
 @include('layouts.piegraph')
