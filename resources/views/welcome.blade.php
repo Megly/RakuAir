3c59048af0090815c0f45bd30a48e7bfd035fd3e
tatsuma.kato <ec2-user@ip-172-31-22-12.us-east-2.compute.internal>
2018-07-12 02:55:51 +0000

    fixed welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,30 +1,15 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>RakuAir</title>
-
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+@section('content')
+    @if (Auth::check())
+       @include('layouts.feel')
         
-        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet' type='text/css'>
-        
-        <link rel="stylesheet" href="{{ secure_asset('css/stylesheet.css') }}">
-    </head>
-    <body>
-     
+    @else
             <div class="text-center">
                 <h1>RakuAir (insert logo pic)</h1><br>
                 {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
                 <p>or</p>
                 {!! link_to_route('login', 'Login!', null, ['class' => 'btn btn-lg btn-success']) !!}
             </div>
-            
-    </body>
-</html>
+    @endif
+@endsection
