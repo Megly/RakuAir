ee871b99d83bc562e8d329b255f4b258f386843a
Jiro.kirameki <ec2-user@ip-172-31-19-4.us-east-2.compute.internal>
2018-07-13 01:42:06 +0000

    css seiri

--- resources/views/layouts/feel.blade.php
+++ resources/views/layouts/feel.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class='feelpage'>
    <div class='hotcontent'>
-           <div class='square_btn'>
+           <div class='hotbtn'>
                 {!! Form::open(['route' => ['hot'], 'method' => 'put']) !!}
                 {!! Form::submit('HOT', ['class' => 'btn btn-danger btn-lg btn-block']) !!}
                 {!! Form::close() !!}
@@ -15,7 +15,7 @@
            </div>
    </div>
    <div class='coldcontent'>
-           <div class='square_btn'>
+           <div class='coldbtn'>
                 {!! Form::open(['route' => ['cold' ], 'method' => 'put']) !!}
                 {!! Form::submit('COLD', ['class' => 'btn btn-primary btn-lg btn-block']) !!}
                 {!! Form::close() !!}
