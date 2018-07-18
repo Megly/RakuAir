2353292b9dd5e622c25e05614ffc49f131527148
tatsuma.kato <ec2-user@ip-172-31-22-12.us-east-2.compute.internal>
2018-07-18 04:32:51 +0000

    sum by team

---
+++ resources/views/teammaps/pink.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@include('commons.navbar')
+
+@section('content')
+
+
+<?php
+$i = 0;
+
+ ${"team".$i} = NULL;
+for ($i=21; $i<34; $i++){
+ ${"team".$i} = App\User::all()->where('team',$i);
+ 	 ${"sum".$i} = 0;
+ 	foreach( ${"team".$i} as $feelings){
+	${"sum".$i} = ${"sum".$i} + $feelings->feel;
+}
+     if(${"sum".$i}>=10)
+	    echo '<p style="background-color:#ff8e8e !important">hot</p>';
+     elseif( 10 > ${"sum".$i} && ${"sum".$i} >= 5)
+	     echo '<p style="background-color:#f9bdbd !important">hot</p>';
+     elseif( -5 >= ${"sum".$i} && ${"sum".$i} >= -10)
+	     echo '<p style="background-color:#bdd2f9 !important">cold</p>';
+     elseif(${"sum".$i} <= -10)
+	     echo '<p style="background-color:#8ec6ff !important">cold</p>';
+     else
+	     echo '<p style="background-color:#f7fff7 !important">comfortable</p>';
+    
+    
+};
+
+	?>
+
+
+
+
+@endsection
