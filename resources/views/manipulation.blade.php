b6211cc021163d0fec7a8e64dd7704aae2a94704
Megly <ec2-user@ip-172-31-11-140.us-east-2.compute.internal>
2018-07-20 02:34:23 +0000

    manipulation page css

--- resources/views/manipulation.blade.php
+++ resources/views/manipulation.blade.php
@@ -11,15 +11,16 @@
 <div class='manipulationpage'>
     <div class="explanation">各エリアの温度調整／リモコンの操作方法</div>
         <div class="asakai">
-            <h1>Asakai Theatre A</h1>
+            <h1>見取り図（Asakai Theatre A）</h1><br>
                 <p>リモコンの色が各エリア（トライブ）の色に相当します。</p>
                 <p>※緑のエリアはトライブが重なっている部分を示しています。</p>
                 <p>（例）PinkトライブのエリアB　→　黄色/緑のリモコン</p>
                 <img class="map" src="/images/Map.png"></img><br><br><br><br><br><br><br>
         </div>
+    
         
         <div class="remote">
-            <h1>リモコンの見方</h1>
+            <h1>リモコンの見方</h1><br>
                 <p>赤枠内のボタンが各エリアに相当します。</p>
                 <p>（例）エリアC　→　①Cボタンをpush　②「室温調節」ボタンで温度調節</p><br>
                 <div class=remotes>
@@ -27,6 +28,6 @@
                     <img class="remote2" src="/images/RemoteOpen.jpg"></img>
                 </div>
         </div>
-
+    </div>
 </div>
 @endsection
