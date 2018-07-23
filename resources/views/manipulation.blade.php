69c0599e476455640c38c741960730ba58a6c0d2
shota.hanaoka <ec2-user@ip-172-31-11-154.us-east-2.compute.internal>
2018-07-23 03:36:55 +0000

    manipulation fix by shota

--- resources/views/manipulation.blade.php
+++ resources/views/manipulation.blade.php
@@ -9,21 +9,21 @@
 </div>
 
 <div class='manipulationpage'>
-    <div class="explanation">How can we adjust temperature in each areas?</div>
+    <div class="explanation">How to adjust temperature in each areas.</div>
         <div class="asakai">
             <h1>Sketch（Asakai Theatre A）</h1><br>
-                <p>The colors of the remote controllers equivalent to the colors of the areas (tribes).</p>
-                <p>*The area of green stands for the area which 2 tribes are mixed.</p>
-                <p>Ex.) area B of Pink tribe >>> Go to the yellow/green controller. </p>
+                <p>The colors of the remote controllers is equal to the colors of the areas (tribes).</p>
+                <p>*The green area is where 2 tribes are mixed.</p>
+                <p>Ex.) Green area of Pink tribe >>> Go to the yellow/green controller. </p>
                 <img class="map" src="/images/Map.png"></img><br><br><br><br><br><br><br>
         </div>
     
         
         <div class="remote">
-            <h1>How to manipulate the controllers</h1><br>
-                <p>Each buttoms in the red line equivalent to the areas shown on the map.</p>
+            <h1>How to manipulate the controllers.</h1><br>
+                <p>Each buttons in the red line is equal to the areas shown on the map.</p>
                 <p>*Not all the controllers have A-G.</p>
-                <p>Ex.) area C >>> 1.Press the buttom "C" twice.  2.Press the buttom "室温調節" to adjust temperature.</p>
+                <p>Ex.) Area C >>> 1.Press the buttom "C" twice.  2.Press the buttom "室温調節" to adjust temperature.</p>
                 <div class=remotes>
                     <img class="remote1" src="/images/RemoteClose.jpg"></img>
                     <img class="remote2" src="/images/RemoteOpen.jpg"></img>
