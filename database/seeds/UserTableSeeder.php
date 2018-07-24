a26bb29c01a94969ce203d4a43a9968ae4317f75
tatsuma.kato <ec2-user@ip-172-31-22-12.us-east-2.compute.internal>
2018-07-24 04:30:56 +0000

    seed

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,16 +11,17 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i <= 300; $i++) {
+        for ($a = 1; $a <=45; $a++) {
+        for ($i = 1; $i <= 6; $i++) {
         DB::table('users')->insert([
             'name' => str_random(10),
             'password' => bcrypt('secret'),
-            'team' => rand(1,45),
+            'team' => $a,
             'sex' => rand(0,1),
             'size' => rand(0,2),
             'feel' => rand(-6,6),
         
-        ]);
+        ]);};
         }
     }
 }
