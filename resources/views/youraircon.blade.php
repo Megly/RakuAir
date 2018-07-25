4ef50b107f32bba9f7cc9428d76c7ae5b02e14f5
Megly <ec2-user@ip-172-31-11-140.us-east-2.compute.internal>
2018-07-25 04:35:24 +0000

    unko

--- resources/views/youraircon.blade.php
+++ resources/views/youraircon.blade.php
@@ -5,7 +5,7 @@ $userteam = $user->team;
 
 <div class = 'airconbtnparent'>
         <a href="{{ route('manipulation.get')}}">
-            <?php echo "$user->name" ?>s's air conditioner is 
+            Your air conditioner is
              <div class='aircon'>
                 @if($userteam == 1 ||   $userteam == 2 || $userteam == 4)
                
