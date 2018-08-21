55e89b8da034a208fc99713226d0de63f5ca754c
taiki.yasuda <ec2-user@ip-172-31-19-4.us-east-2.compute.internal>
2018-08-21 09:26:21 +0000

    mickey

--- app/Http/Controllers/ComController.php
+++ app/Http/Controllers/ComController.php
@@ -72,18 +72,8 @@ class ComController extends Controller
         $user ->feel=0;
         $user->save();
         
-        if($user->team >= 1 && $user->team <=9 ){
-            return view('teammaps.red');
-        }
-        elseif($user->team >= 10 && $user->team <= 21){
-            return view('teammaps.yellow');
-        }
-        elseif($user->team >= 22 && $user->team <= 33){
-            return view('teammaps.pink');
-        }
-        elseif($user->team >= 34 && $user->team <= 45){
-            return view('teammaps.purple');
-        }
+        return view('map');
+        
     }
 
     /**
