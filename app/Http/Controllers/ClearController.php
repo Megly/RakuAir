b0732e1d92e9d88c35269cc2895131535aa64a4f
taiki.yasuda <ec2-user@ip-172-31-19-4.us-east-2.compute.internal>
2018-08-30 00:43:24 +0000

    reset blade clearcontroller

--- app/Http/Controllers/ClearController.php
+++ app/Http/Controllers/ClearController.php
@@ -7,11 +7,11 @@ use App\User;
 
 class ClearController extends Controller
 {
- // Area3のエアコン
-    public function d3()
+ // 楽市のエアコン、１から１６まで
+    public function a1()
     {
-     $users1 = User::all()->where('team','4');
-     $users2 = User::all()->where('team','>=','1')->where('team','<=','2');
+     $users1 = User::all()->where('team','1');
+     $users2 = User::all()->where('team','7');
      foreach($users1 as $user){
      $user -> feel = 0;
      $user -> save();
@@ -23,45 +23,9 @@ class ClearController extends Controller
      return view('map');
     }
     
-    public function b3()
+    public function b1()
     {
-     $users1 = User::all()->where('team','3');
-     $users2 = User::all()->where('team','>=','5')->where('team','<=','6');
-     foreach($users1 as $user){
-     $user -> feel = 0;
-     $user -> save();
-     };
-     foreach($users2 as $user){
-     $user -> feel = 0;
-     $user -> save();
-     };
-     return view('map');
-    }
-    
-    public function c3()
-    {
-     $users = User::all()->where('team','7');
-     foreach($users as $user){
-     $user -> feel = 0;
-     $user -> save();
-     };
-     return view('map');
-    }
-    
-    public function a3()
-    {
-     $users = User::all()->where('team','>=','8')->where('team','<=','9');
-     foreach($users as $user){
-     $user -> feel = 0;
-     $user -> save();
-     };
-     return view('map');
-    }
-    
-    // Area2のエアコン
-    public function d2()
-    {
-     $users = User::all()->where('team','>=','10')->where('team','<=','15');
+     $users = User::all()->where('team','12');
      foreach($users as $user){
      $user -> feel = 0;
      $user -> save();
@@ -71,17 +35,20 @@ class ClearController extends Controller
     
     public function c2()
     {
-     $users = User::all()->where('team','>=','16')->where('team','<=','21');
-     foreach($users as $user){
+     $users1 = User::all()->where('team','>=','2')->where('team','<=','4');
+     $users2 = User::all()->where('team','>=','8')->where('team','<=','10');
+     $users = [$users1,$users2];
+     foreach($users as $a){
+     foreach($a as $user){
      $user -> feel = 0;
      $user -> save();
-     };
+     };};
      return view('map');
     }
     
-    public function b2()
+    public function d2()
     {
-     $users = User::all()->where('team','>=','22')->where('team','<=','27');
+     $users1 = User::all()->where('team','>=','13')->where('team','<=','15');
      foreach($users as $user){
      $user -> feel = 0;
      $user -> save();
@@ -91,19 +58,39 @@ class ClearController extends Controller
     
     public function a2()
     {
-     $users = User::all()->where('team','>=','28')->where('team','<=','33');
-     foreach($users as $user){
+     $users1 = User::all()->where('team','>=','5')->where('team','<=','6');
+     $users2 = User::all()->where('team','11');
+     $users3 = User::all()->where('team','21');
+     $users = [$users1,$users2,$users3];
+     foreach($users as $a){
+     foreach($a as $user){
+     $user -> feel = 0;
+     $user -> save();
+     };};
+     return view('map');
+    }
+    
+    public function b2()
+    {
+     $users1 = User::all()->where('team','16');
+     $users2 = User::all()->where('team','27');
+     foreach($users1 as $user){
+     $user -> feel = 0;
+     $user -> save();
+     };
+     foreach($users2 as $user){
      $user -> feel = 0;
      $user -> save();
      };
      return view('map');
     }
     
-    // Area1のエアコン
-    public function d1()
+    
+    // 楽座のエアコン１７から３２
+    public function a3()
     {
-     $users1 = User::all()->where('team','>=','34')->where('team','<=','35');
-     $users2 = User::all()->where('team','>=','38')->where('team','<=','39');
+     $users1 = User::all()->where('team','>=','17')->where('team','<=','19');
+     $users2 = User::all()->where('team','>=','22')->where('team','<=','24');
      $users = [$users1,$users2];
      foreach($users as $a){
      foreach($a as $user){
@@ -113,9 +100,9 @@ class ClearController extends Controller
      return view('map');
     }
     
-    public function b1()
+    public function b3()
     {
-     $users = User::all()->where('team','>=','36')->where('team','<=','37');
+     $users = User::all()->where('team','>=','28')->where('team','<=','30');
      foreach($users as $user){
      $user -> feel = 0;
      $user -> save();
@@ -123,20 +110,10 @@ class ClearController extends Controller
      return view('map');
     }
     
-    public function c1()
+    public function c3()
     {
-     $users = User::all()->where('team','>=','42')->where('team','<=','43');
-     foreach($users as $user){
-     $user -> feel = 0;
-     $user -> save();
-     };
-     return view('map');
-    }
-    
-    public function a1()
-    {
-     $users1 = User::all()->where('team','>=','40')->where('team','<=','41');
-     $users2 = User::all()->where('team','>=','44')->where('team','<=','45');
+     $users1 = User::all()->where('team','20');
+     $users2 = User::all()->where('team','>=','25')->where('team','<=','26');
      $users = [$users1,$users2];
      foreach($users as $a){
      foreach($a as $user){
@@ -146,4 +123,13 @@ class ClearController extends Controller
      return view('map');
     }
     
+    public function d3()
+    {
+     $users = User::all()->where('team','>=','31')->where('team','<=','32');
+     foreach($users as $user){
+     $user -> feel = 0;
+     $user -> save();
+     };
+     return view('map');
+    }
 }
--- resources/views/reset.blade.php
+++ resources/views/reset.blade.php
@@ -5,89 +5,75 @@ $userteam = $user->team;
         
 
 <div class = 'clearbtnparent'>
-        @if($userteam == 1 || $userteam == 2 || $userteam == 4)
+        @if($userteam == 1 || $userteam == 7)
         <div class='clearbtn'>
-                {!! Form::open(['route' => ['clear3d' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
+                {!! Form::open(['route' => ['clear1a' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
                 {!! Form::submit('⚠︎Adjusted', ['class' => 'glyphicon glyphicon-warning-sign clearbtnparent blink-adjusthot btn-block']) !!}
                 {!! Form::close() !!}
         </div>
-        @elseif($userteam == 3 || $userteam == 5 || $userteam == 6)
+        @elseif($userteam == 12)
         <div class='clearbtn'>
-                {!! Form::open(['route' => ['clear3b' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
+                {!! Form::open(['route' => ['clear1b' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
                 {!! Form::submit('⚠︎Adjusted', ['class' => 'clearbtnparent blink-adjusthot btn-block']) !!}
                 {!! Form::close() !!}
         </div>
-        @elseif($userteam == 7)
-        <div class='clearbtn'>
-                {!! Form::open(['route' => ['clear3c' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
-                {!! Form::submit('⚠︎Adjusted', ['class' => 'clearbtnparent blink-adjusthot btn-block']) !!}
-                {!! Form::close() !!}
-        </div>
-        @elseif($userteam == 8 || $userteam == 9)
-        <div class='clearbtn'>
-                {!! Form::open(['route' => ['clear3a' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
-                {!! Form::submit('⚠︎Adjusted', ['class' => 'clearbtnparent blink-adjusthot btn-block']) !!}
-                {!! Form::close() !!}
-</div>
-        
-        
-<div class = 'clearbtnparent'>
-        @elseif($userteam == 10 || $userteam == 11 || $userteam == 12 || $userteam == 13 || $userteam == 14 || $userteam == 15)        
-        <div class='clearbtn'>
-                {!! Form::open(['route' => ['clear2d' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
-                {!! Form::submit('⚠︎Adjusted', ['class' => 'clearbtnparent blink-adjusthot btn-block']) !!}
-                {!! Form::close() !!}
-        </div>
-        @elseif($userteam == 16 || $userteam == 17 || $userteam == 18 || $userteam == 19 || $userteam == 20 || $userteam == 21)
+        @elseif($userteam == 2 || $userteam == 3 || $userteam == 4 || $userteam == 8 || $userteam == 9 || $userteam == 10)
         <div class='clearbtn'>
                 {!! Form::open(['route' => ['clear2c' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
                 {!! Form::submit('⚠︎Adjusted', ['class' => 'clearbtnparent blink-adjusthot btn-block']) !!}
                 {!! Form::close() !!}
         </div>
-        @elseif($userteam == 22 || $userteam == 23 || $userteam == 24 || $userteam == 25 || $userteam == 26 || $userteam == 27)
+        @elseif($userteam == 13 || $userteam == 14 || $userteam == 15)
         <div class='clearbtn'>
-                {!! Form::open(['route' => ['clear2b' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
+                {!! Form::open(['route' => ['clear2d' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
                 {!! Form::submit('⚠︎Adjusted', ['class' => 'clearbtnparent blink-adjusthot btn-block']) !!}
                 {!! Form::close() !!}
         </div>
-        @elseif($userteam == 28 || $userteam == 29 || $userteam == 30 || $userteam == 31 || $userteam == 32 || $userteam == 33)
+        
+        @elseif($userteam == 5 || $userteam == 6 || $userteam == 11 || $userteam == 21)
         <div class='clearbtn'>
                 {!! Form::open(['route' => ['clear2a' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
                 {!! Form::submit('⚠︎Adjusted', ['class' => 'clearbtnparent blink-adjusthot btn-block']) !!}
                 {!! Form::close() !!}
         </div>
         
+        @elseif($userteam == 16 || $userteam == 27)
+        <div class='clearbtn'>
+                {!! Form::open(['route' => ['clear2b' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
+                {!! Form::submit('⚠︎Adjusted', ['class' => 'clearbtnparent blink-adjusthot btn-block']) !!}
+                {!! Form::close() !!}
+        </div>
 </div>
+        
         
 <div class = 'clearbtnparent'>
-        @elseif($userteam == 34 || $userteam == 35 || $userteam == 38 || $userteam == 39)
+        @elseif($userteam == 17 || $userteam == 18 || $userteam == 19 || $userteam == 22 || $userteam == 23 || $userteam == 24)
         <div class='clearbtn'>
-                {!! Form::open(['route' => ['clear1d' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
+                {!! Form::open(['route' => ['clear3a' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
                 {!! Form::submit('⚠︎Adjusted', ['class' => 'clearbtnparent blink-adjusthot btn-block']) !!}
                 {!! Form::close() !!}
         </div>
-        @elseif($userteam == 36 || $userteam == 37)
+        @elseif($userteam == 28 || $userteam == 29 || $userteam == 30)
         <div class='clearbtn'>
-                {!! Form::open(['route' => ['clear1b' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
+                {!! Form::open(['route' => ['clear3b' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
                 {!! Form::submit('⚠︎Adjusted', ['class' => 'clearbtnparent blink-adjusthot btn-block']) !!}
                 {!! Form::close() !!}
         </div>
-        @elseif($userteam == 42 || $userteam == 43)
+        @elseif($userteam == 20 || $userteam == 25 || $userteam == 26)
         <div class='clearbtn'>
-                {!! Form::open(['route' => ['clear1c' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
+                {!! Form::open(['route' => ['clear3c' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
                 {!! Form::submit('⚠︎Adjusted', ['class' => 'clearbtnparent blink-adjusthot btn-block']) !!}
                 {!! Form::close() !!}
         </div>
-        @elseif($userteam == 40 || $userteam == 41 || $userteam == 44 || $userteam == 45)
+        @elseif($userteam == 31 || $userteam == 32)
         <div class='clearbtn'>
-                {!! Form::open(['route' => ['clear1a' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
+                {!! Form::open(['route' => ['clear3d' ], 'method' => 'put', 'onSubmit' => 'return checkSubmit()']) !!}
                 {!! Form::submit('⚠︎Adjusted', ['class' => 'clearbtnparent blink-adjusthot btn-block']) !!}
                 {!! Form::close() !!}
         </div>
         
 </div>
         
-         
 
 @endif
 
