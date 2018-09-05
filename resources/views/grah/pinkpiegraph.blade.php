d661eec7210c9886b729659297f8cf51025ab2b6
taiki.yasuda <ec2-user@ip-172-31-19-4.us-east-2.compute.internal>
2018-09-05 03:15:31 +0000

    C and D delete

--- resources/views/grah/pinkpiegraph.blade.php
+++ resources/views/grah/pinkpiegraph.blade.php
@@ -1,29 +1,29 @@
-<div class="result-top">
-<div class="grah">
-<script type="text/javascript">
-google.load("visualization", "1", {packages:["corechart"]});
-google.setOnLoadCallback(drawChart);
-function drawChart() {
-var data = google.visualization.arrayToDataTable([ //グラフデータの指定
-        ['Task', 'Hours per Day'],
-        ['Cold',    <?php echo $countpinkcold ?>],
-        ['Hot',     <?php echo $countpinkhot ?>],
-        ['Comfortable',     <?php echo $countcompink ?>]
+<!--<div class="result-top">-->
+<!--<div class="grah">-->
+<!--<script type="text/javascript">-->
+<!--google.load("visualization", "1", {packages:["corechart"]});-->
+<!--google.setOnLoadCallback(drawChart);-->
+<!--function drawChart() {-->
+<!--var data = google.visualization.arrayToDataTable([ //グラフデータの指定-->
+<!--        ['Task', 'Hours per Day'],-->
+<!--        ['Cold',    <?php echo $countpinkcold ?>],-->
+<!--        ['Hot',     <?php echo $countpinkhot ?>],-->
+<!--        ['Comfortable',     <?php echo $countcompink ?>]-->
         
      
        
-]);
-var options = { //オプションの指定
-         title: 'C',
-        'backgroundColor': {'fill':'#fff'},
-        width: 500,
-                height: 300,
-};
-var chart = new google.visualization.PieChart(document.getElementById('pinkpiechart')); //グラフを表示させる要素の指定
-    chart.draw(data, options);
-}
-</script>
-<div id="pinkpiechart"></div>
+<!--]);-->
+<!--var options = { //オプションの指定-->
+<!--         title: 'C',-->
+<!--        'backgroundColor': {'fill':'#fff'},-->
+<!--        width: 500,-->
+<!--                height: 300,-->
+<!--};-->
+<!--var chart = new google.visualization.PieChart(document.getElementById('pinkpiechart')); //グラフを表示させる要素の指定-->
+<!--    chart.draw(data, options);-->
+<!--}-->
+<!--</script>-->
+<!--<div id="pinkpiechart"></div>-->
 
-</div>
-</div>
+<!--</div>-->
+<!--</div>-->
--- resources/views/grah/purplepiegraph.blade.php
+++ resources/views/grah/purplepiegraph.blade.php
@@ -1,29 +1,29 @@
-<div class="result-top">
-<div class="grah">
-<script type="text/javascript">
-google.load("visualization", "1", {packages:["corechart"]});
-google.setOnLoadCallback(drawChart);
-function drawChart() {
-var data = google.visualization.arrayToDataTable([ //グラフデータの指定
-        ['Task', 'Hours per Day'],
-        ['Cold',    <?php echo $countpurplecold ?>],
-        ['Hot',     <?php echo $countpurplehot ?>],
-        ['Comfortable',     <?php echo $countcompurple ?>]
+<!--<div class="result-top">-->
+<!--<div class="grah">-->
+<!--<script type="text/javascript">-->
+<!--google.load("visualization", "1", {packages:["corechart"]});-->
+<!--google.setOnLoadCallback(drawChart);-->
+<!--function drawChart() {-->
+<!--var data = google.visualization.arrayToDataTable([ //グラフデータの指定-->
+<!--        ['Task', 'Hours per Day'],-->
+<!--        ['Cold',    <?php echo $countpurplecold ?>],-->
+<!--        ['Hot',     <?php echo $countpurplehot ?>],-->
+<!--        ['Comfortable',     <?php echo $countcompurple ?>]-->
         
      
        
-]);
-var options = { //オプションの指定
-         title: 'D',
-        'backgroundColor': {'fill':'#fff'},
-        width: 500,
-                height: 300,
-};
-var chart = new google.visualization.PieChart(document.getElementById('purplepiechart')); //グラフを表示させる要素の指定
-    chart.draw(data, options);
-}
-</script>
-<div id="purplepiechart"></div>
+<!--]);-->
+<!--var options = { //オプションの指定-->
+<!--         title: 'D',-->
+<!--        'backgroundColor': {'fill':'#fff'},-->
+<!--        width: 500,-->
+<!--                height: 300,-->
+<!--};-->
+<!--s
+<!--    chart.draw(data, options);-->
+<!--}-->
+<!--</script>-->
+<!--<div id="purplepiechart"></div>-->
 
-</div>
-</div>
+<!--</div>-->
+<!--</div>-->
